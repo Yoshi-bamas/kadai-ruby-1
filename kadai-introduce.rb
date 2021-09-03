ecc92c22681ae3ef2736921360b672b95f79a881
yoshiya.oba <yoshirinpana@gmail.com>
2021-09-03 09:29:20 +0000

    修正版です。

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,5 +1,4 @@
 family_name = "Oba"
 first_name = "Yoshiya"
 age = 24
-introduce = family_name + first_name + "です。"+age.to_s+"歳です。"
-puts introduce
+puts family_name + first_name + "です。"+age.to_s+"歳です。"
